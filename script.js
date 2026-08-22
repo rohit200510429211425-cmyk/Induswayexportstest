b598f99788a5213ced37745e74cce3b8dce7c578
rohit200510429211425-cmyk <rohit200510429211425@gmail.com>
2026-08-23 01:50:17 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-const toggle=document.querySelector('.menu-toggle'),nav=document.querySelector('nav');
-toggle.addEventListener('click',()=>{const open=nav.classList.toggle('open');toggle.setAttribute('aria-expanded',open)});
-document.querySelectorAll('nav a').forEach(link=>link.addEventListener('click',()=>{nav.classList.remove('open');toggle.setAttribute('aria-expanded','false')}));
